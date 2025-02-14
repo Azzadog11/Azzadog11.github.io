eabc45b7e2470a60b680c3f5ddaff367d78ea36d
Aaron <77808592+Azzadog11@users.noreply.github.com>
2025-02-14 00:31:30 +0000

    Updating

--- src/App.jsx
+++ src/App.jsx
@@ -89,13 +89,9 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
       target="__blank"
     >
-      Made with{" "}
-      <span role="img" aria-label="heart">
-        ❤️
-      </span>
+      Made with help from the internet by Aaron...
     </a>
   );
 };
